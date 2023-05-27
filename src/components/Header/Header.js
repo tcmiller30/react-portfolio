890ae2068ef2d5d7c4a47ddca3aed48d978f1bbd
Travis <traviscmiller01@gmail.com>
2023-05-27 13:10:22 -0700

    added js, css, and index.js files to components folders

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header-bg w-50'>
+        <div className=" fs-1">Header</div>
+    </div>
+  )
+}
+
+export default Header
